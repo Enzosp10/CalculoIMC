6ec888346f04f7a28abb8cb6e54b1622ab19fbfa
Enzo <enzo.virginiogomez.cl>
2021-09-24 12:19:07 -0300

    tercera prueba 24 09 21

--- app/src/main/java/com/example/calculoimc/MainActivity.java
+++ app/src/main/java/com/example/calculoimc/MainActivity.java
@@ -2,16 +2,32 @@ package com.example.calculoimc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btnAceptar;
+    private EditText eTpeso,eTAltura;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
        setContentView(R.layout.activity_main);
-
-
+       final Context context = getApplicationContext();
+       btnAceptar = (Button)  findViewById(R.id.btnAceptar);
+       eTpeso = (EditText) findViewById(R.id.eTpeso);
+       eTAltura = (EditText) findViewById(R.id.eTAltura);
+        btnAceptar.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               //resultado = peso/(altura*altura)
+               Toast.makeText(context, "Click en el Boton Aceptar ", Toast.LENGTH_SHORT).show();
+           }
+       });
 
     }
 }
